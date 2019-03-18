3a1de7c46c550f8ad23cf8b6dee4e0afb62fc6f2
ylee02 <ylee01@stuy.edu>
2019-03-19 08:18:50 +0900

    partition works

--- Quick.java
+++ Quick.java
@@ -1,4 +1,11 @@
+import java.util.*;
 public class Quick{
+	
+	public static void main(String[] args) {
+		int[] data = {3, 4, 5, 6, 1, 2, 9, 0};
+		System.out.println(partition(data, 0, 7));
+		System.out.println(Arrays.toString(data));
+	}
 	
 	public static int partition(int[] data, int start, int end) {
 		Random randgen = new Random();
@@ -10,10 +17,19 @@ public class Quick{
 	}
 	
 	public static int partitionH(int[] data, int start, int end, int pivot) {
+		System.out.println(Arrays.toString(data));
+		System.out.println(start);
 		if (start == end) {
-			data[0] = data[start];
-			data[start] = pivot;
-			return start;
+			if (data[start] < pivot) {
+				data[0] = data[start];
+				data[start] = pivot;
+				return start;
+			}else {
+				data[0] = data[start - 1];
+				data[start - 1] = pivot;
+				return start -1;
+			}
+			
 		}
 		if (data[start] > pivot) {
 			int temp = data[end];
@@ -21,8 +37,11 @@ public class Quick{
 			data[start] = temp;
 			return partitionH(data, start, end - 1, pivot);
 		}
-		return partitonH(data, start + 1, end, pivot);
+		return partitionH(data, start + 1, end, pivot);
 	}
 	
 	public static int quickselect(int[] data, int k) {
+		return -1;
+	}
+}
 		
