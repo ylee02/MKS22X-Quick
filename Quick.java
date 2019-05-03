5cc9bf6d7dfd5b68ab9633498041536469b7ef7d
ylee02 <ylee01@stuy.edu>
2019-05-03 14:04:10 +0900

    this took long too

--- Quick.java
+++ Quick.java
@@ -8,7 +8,7 @@ public class Quick{
 		
 	}
 	
-	public static int partition(int[] data, int start, int end) {
+	/*public static int partition(int[] data, int start, int end) {
 		Random randgen = new Random();
 		int part = randgen.nextInt(end - start + 1) + start;
 		
@@ -40,7 +40,27 @@ public class Quick{
 			return partitionH(data, start, end - 1, pivot);
 		}
 		return partitionH(data, start + 1, end, pivot);
-	}
+	}*/
+	
+	public static int partition(int[] data, int start, int end) {
+        int pivot = data[start]; 
+        while (start <= end) {
+            while (data[start] < pivot) {
+                start++;
+            }
+            while (data[end] > pivot) {
+                end--;
+            }
+            if (start <= end) {
+                int temp = data[start];
+                data[start] = data[end];
+                data[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        return start;
+    }
 	
 	public static int[] copy(int[] data) {
 		int[] temp = new int[data.length];
@@ -60,23 +80,41 @@ public class Quick{
 	}
 	
 	public static int quickselect(int[] data, int k) {
-		int[] temp = copy(data);
-		for (int i = 0; i < temp.length; i++) {
-			
-			data[indexOf(data, temp[i])] = data[0];
-			data[0] = temp[i];
-			if (partitionH(data, 0, data.length - 1, temp[i]) == k) {
-				return data[k];
-			}
-		}
-		return -1;
+		return quickselecth(data, 0, data.length -1, k) + 1;
 			
 		
 	}
 	
+	public static int quickselecth(int[] data, int start, int end, int k) {
+		if (k > 0 && k <= end - start + 1) {
+			int temp = data[end];
+			int pivot = start;
+			for (int i = start; i <= end - 1; i++) {
+				if (data[i] <= temp) {
+					int temp3 = data[i];
+					data[i] = data[pivot];
+					data[pivot] = temp3;
+					pivot++;
+				}
+			}
+			int temp3 = data[pivot];
+			data[pivot] = data[end];
+			data[end] = temp3;
+			
+			if (pivot - start == k -1) {
+				return data[pivot];
+			}
+			if (pivot - start > k - 1) {
+				return quickselecth(data, start, pivot - 1, k);
+			}
+			return quickselecth(data, pivot + 1, end, k - pivot + start - 1);
+		}
+		return -1;
+	}
+	
 	
 	public static void quicksort(int[] data) {
-		int[] temp = new int[data.length];
+		/*int[] temp = new int[data.length];
 		for (int i = 0; i < data.length; i++) {
 			
 			temp[i] = quickselect(data, i);
@@ -86,6 +124,20 @@ public class Quick{
 		}
 		for (int i = 0; i < data.length; i++) {
 			data[i] = temp[i];
+		}*/
+		
+		quicksorth(data, 0, data.length - 1);
+		
+	}
+	
+	public static void quicksorth(int[] data, int start, int end) {
+		int pivot = partition(data, start, end);
+		
+		if (start < pivot - 1) {
+			quicksorth(data, start, pivot - 1);
+		}
+		if (end > pivot) {
+			quicksorth(data, pivot, end);
 		}
 	}
 	
